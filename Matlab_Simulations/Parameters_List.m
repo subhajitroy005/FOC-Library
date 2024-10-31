211a201c628df0111f03de2576aaae1c6b753eaf
Subhajit Roy <subhajit.roy@fluxauto.xyz>
2024-10-31 21:46:03 +0530

    Hysteresis Control Mechanical block added.

---
+++ Matlab_Simulations/Parameters_List.m
@@ -0,0 +1,14 @@
+%% Machine Parameters
+pole_pair_number    = 4;        % Number of pole pairs
+Rs                  = 0.1;      % Stator resistance per phase           [Ohm]
+Ls                  = 1e-4;     % Stator self-inductance per phase, Ls  [H]
+Ms                  = 1e-5;     % Stator mutual inductance, Ms          [H]
+psim                = 0.0175;   % Maximum permanent magnet flux linkage [Wb]
+Jm                  = 0.0005;   % Rotor inertia                         [Kg*m^2]
+Vdc                 = 48;       % DC link voltage                       [V]
+
+%% Control Parameters
+Ts                  = 1e-4;  % Fundamental sample time            [s]
+Tsc                 = 1e-4;  % Sample time for inner control loop [s]
+iref                = 70;    % Reference current                  [A]
+h                   = 1;     % Hysteresis band                    [A]
